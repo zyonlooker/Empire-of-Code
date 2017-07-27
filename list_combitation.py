ece71717decba29c8c60172c41ade05e323ada6d
zhangy78 <zhangy78@localhost.localdomain>
2017-07-27 22:57:23 +0100

    List Combitation

---
+++ list_combitation.py
@@ -0,0 +1,13 @@
+def list_combination(list1, list2):
+    list = []
+    for i in range(len(list1)):
+        list.append(list1[i])
+        list.append(list2[i])
+    return list
+
+if __name__ == '__main__':
+    assert list_combination([1, 2, 3], [4, 5, 6]) == [1, 4, 2, 5, 3, 6], "First"
+    assert list_combination([1, 1, 1, 1], [2, 2, 2, 2]) == [1, 2, 1, 2, 1, 2, 1, 2], "Second"
+
+    print("All set? Click \"Check\" to review your code and earn rewards!")
+
